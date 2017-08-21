cd4c0dcdba768253f8c0d8d6c61d160f01841a50
mokamoto12 <mokamoto1208@gmail.com>
2017-08-22 00:09:35 +0900

    Resolve Problem2
    https://projecteuler.net/problem=2

--- bin/index.php
+++ bin/index.php
@@ -3,6 +3,8 @@
 require __DIR__.'/../bootstrap/autoload.php';
 
 use Mokamoto12\ProjectEuler\Application;
+use Mokamoto12\ProjectEuler\Problem\Sequence\FibonacciSequence;
+use Mokamoto12\ProjectEuler\Problem\Specification\EvenSpecification;
 use Mokamoto12\ProjectEuler\Problem\SumProblem;
 use Mokamoto12\ProjectEuler\Problem\Sequence\NaturalSequence;
 use Mokamoto12\ProjectEuler\Problem\Specification\MultipleSpecification;
@@ -11,3 +13,8 @@ $problem = new SumProblem(new NaturalSequence(1000), (new MultipleSpecification(
 
 $app = new Application($problem);
 $app->run();
+
+$problem2 = new SumProblem(new FibonacciSequence(4000000), new EvenSpecification());
+
+$app2 = new Application($problem2);
+$app2->run();
