1006aa4b90aa33015964d871b0bb75065042875f
mokamoto12 <mokamoto1208@gmail.com>
2017-08-22 00:55:14 +0900

    Add benchmark

--- bin/index.php
+++ bin/index.php
@@ -2,6 +2,7 @@
 
 require __DIR__.'/../bootstrap/autoload.php';
 
+use Lavoiesl\PhpBenchmark\Benchmark;
 use Mokamoto12\ProjectEuler\Application;
 use Mokamoto12\ProjectEuler\Problem\Sequence\FibonacciSequence;
 use Mokamoto12\ProjectEuler\Problem\Specification\EvenSpecification;
@@ -10,11 +11,22 @@ use Mokamoto12\ProjectEuler\Problem\Sequence\NaturalSequence;
 use Mokamoto12\ProjectEuler\Problem\Specification\MultipleSpecification;
 
 $problem = new SumProblem(new NaturalSequence(1000), (new MultipleSpecification(3))->or(new MultipleSpecification(5)));
-
-$app = new Application($problem);
-$app->run();
-
 $problem2 = new SumProblem(new FibonacciSequence(4000000), new EvenSpecification());
 
+$benchmark = new Benchmark();
+
+$benchmark->add('Problem 1', function () use ($problem) {
+    $problem->resolve();
+});
+
+$benchmark->add('Problem 2', function () use ($problem2) {
+    $problem2->resolve();
+});
+
+$benchmark->run();
+
+$app = new Application($problem);
 $app2 = new Application($problem2);
+
+$app->run();
 $app2->run();
